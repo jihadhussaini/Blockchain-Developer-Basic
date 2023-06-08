c5e9cc23cc2d935b5a4b17511c65f76a2b56f2df
Jihad Hussaini <jihad.akbar.hussaini@gmail.com>
2023-06-08 08:21:09 +0700

    simple storage contract, remixd connection

---
+++ simpleStorage.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract simpleStorage {
+    uint256 favoriteNumber;
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    People[] public people;
+
+    function store (uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(uint256 _favoriteNumber, string memory _name) public {
+        // first way
+        // People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        // people.push(newPerson);
+
+        // second way
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
